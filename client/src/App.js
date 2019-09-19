0d4cb17072b858ba20768931d5ecd5f20b1fd583
falk <petruk0908@gmail.com>
2019-09-19 18:57:15 +0300

    add scss support

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Привет мир!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
